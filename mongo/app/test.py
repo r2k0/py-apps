ef5d977b3f3edc0251c82705af5f1a7fd5d3b130
r2k0 <r2k0@outlook.com>
2014-10-01 22:30:45 +0000

    update

--- mongo/app/test.py
+++ mongo/app/test.py
@@ -9,6 +9,14 @@ def testMongo():
   print (db.name)
   print (db.my_collection)
 
+def storedb(data):
+  client = pymongo.MongoClient("localhost",27017)
+  db = client.test
+  db.test.drop()
+  for i in data:
+    db.test.insert(i)
+  print (db.test.count())
+
 def parse(raw_file, delimiter):
   """ 
     parses CSV file
@@ -27,8 +35,9 @@ def parse(raw_file, delimiter):
 
 def main():
   spy_data = parse("spy_2013.csv",",")
-  print(spy_data)
-  testMongo()
+  #print(spy_data)
+  storedb(spy_data)
+
 
 if __name__ == "__main__":
   main()
