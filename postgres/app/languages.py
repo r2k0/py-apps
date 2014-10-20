b4546b31591193f79112624d39715bf8967a6e8d
r2k0 <r2k0@outlook.com>
2014-10-20 21:17:22 +0000

    updates

---
+++ postgres/app/languages.py
@@ -0,0 +1,32 @@
+import psycopg2
+import sys
+
+langs = (
+	(1,'Javascript',294832),
+	(2,'Java',176000),
+	(3,'CSS',151000),
+	(4,'Python',142272),
+	(5,'Ruby',132397)
+)
+
+con = None
+
+try:
+  con = psycopg2.connect("dbname='testdb' user='vagrant'")
+  cur = con.cursor()
+  cur.execute("DROP TABLE IF EXISTS Languages")
+  cur.execute("CREATE TABLE Languages(Id INT PRIMARY KEY, Name TEXT, Repos INT)")
+  query = "INSERT INTO Languages (Id, Name, Repos) VALUES (%s,%s,%s)"
+  cur.executemany(query, langs)
+  con.commit()
+  cur.execute("SELECT * FROM Languages")
+  rows = cur.fetchall()
+  for r in rows: print r[0],r[1],r[2]
+
+except psycopg2.DatabaseError, e:
+  if con: con.rollback()
+  print e
+  sys.exit(1)
+
+finally:
+  if con: con.close()
