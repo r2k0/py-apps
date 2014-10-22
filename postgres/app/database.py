0df12629dd689b5d3e7f987ce35ee37a78f4a924
r2k0 <r2k0@outlook.com>
2014-10-22 05:38:19 +0000

    updates

--- postgres/app/database.py
+++ postgres/app/database.py
@@ -8,4 +8,6 @@ db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-
+def init_db()
+  import app.models
+  Base.metadata.create_all(bind=engine)
